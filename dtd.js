1e053068e03a4806df4df083f8140c9425eb9f57
liquidity <dm@deathtrapdungeons.com>
2021-10-24 00:49:18 +1300

    Working UMD

--- dtd.js
+++ dtd.js
@@ -1,45 +1,94 @@
 class DTDShop {
     static init() {
-        // Register dependencies for vue-moveable and vue-soundcloud-player
-        Dlopen.register('Quasar', {
-            scripts: "https://cdn.jsdelivr.net/npm/quasar@1.15.8/dist/quasar.umd.min.js",
-            styles: [
-                "https://cdn.jsdelivr.net/npm/quasar@1.15.8/dist/quasar.min.css",
-				"https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons|Material+Icons+Outlined|Material+Icons+Round",
+		game.settings.register("DTDShop", "debug", {
+            name: game.i18n.localize("DTDShop.debug"),
+            hint: game.i18n.localize("DTDShop.debugHint"),
+            scope: "client",
+            config: true,
+            default: false,
+            type: Boolean
+        });
+    }
 
-	        ],
-			init: () => {
-            	Vue.use(Quasar);
-			}
+	static setup() {
+        const debug = game.settings.get("DTDShop", "debug");
+
+        Dlopen.register('vue', {
+            scripts: `https://cdn.jsdelivr.net/npm/vue@3.2.20/dist/vue.global.js`,
+            init: () => {
+				// This is Vue v 2.0 code.
+				// See https://v3.vuejs.org/guide/plugins.html#using-a-plugin for how to update it.
+                // Vue.config.devtools = debug;              
+                // Expose Foundry objects as global object in Vue plugin
+                // Vue.use({
+                //     install(Vue, options) {
+                //         Vue.prototype.ui = ui;
+                //         Vue.prototype.game = game;
+                //         Vue.prototype.canvas = canvas;
+                //     },
+                // })
+            }
+        });
+        Dlopen.register('vuex', {
+            scripts: `https://unpkg.com/vuex@4.0.0/dist/vuex.global.js`,
+            dependencies: "vue",
+            init: () => Vue.use(Vuex)
         });
 
 		Dlopen.register('magic-shop', {
-			scripts: ["/modules/dtd-shop/dist/dtd-shop-vue.umd.js"],
+			scripts: ["/modules/dtd-shop/dist/dtdShopV3.umd.js"],
 			styles: [
-				"/modules/dtd-shop/dist/dtd-shop-vue.css",
+				"/modules/dtd-shop/dist/dtdShopV3.css",
+				"https://fonts.googleapis.com/icon?family=Material+Icons"
 			],
-			dependencies: ["Quasar"],
+			dependencies: ["vue"],
 			init: () => {
 				console.log("loaded magic-shop");
-				Vue.component('App', window['dtd-shop-vue']);
-		}
+			}
 		});
 
-        // Define dependency on our own custom vue components for when we need it
-		Dlopen.register('dtd-shop', {
-            scripts: "/modules/dtd-shop/dist/vue-components.min.js",
-			dependencies: ["magic-shop"]
-        });
+        const observer = new window.MutationObserver(DTDShop._documentModified.bind(DTDShop));
+        observer.observe(document, { "subtree": true, "childList": true });
+        this._autoRender();
     }
+
+	static async render(template, element, {data = {}, store = null, dependencies=[], renderData}={}) {
+        const vueDeps = ['vue'];
+        if (store) vueDeps.push('vuex');
+        await Dlopen.loadDependencies(vueDeps.concat(dependencies));	
+		return app;
+    }
+
+    static async _autoRender(element) {
+        const components = $(element).find(".vueport-render").addBack(".vueport-render");
+        for (let el of components.toArray()) {
+            const id = el.id || "vueApp";
+            const deps = el.getAttribute("dependencies") || undefined;
+            // Can't change the class after render because the element won't exist anymore and there's no guarantee
+            // that the Vue element itself won't be a comment/lazy-loaded.
+            // Also, we don't want to re-trigger the mutation observer on the vue content
+            el.classList.remove("vueport-render");
+            el.classList.add("vueport-rendered");
+            this[id] = await this.render(null, el, {dependencies: deps && deps.split(" ")});
+        }
+    }
+    static async _documentModified(mutations, observer) {
+        const addedNodes = mutations.reduce((nodes, mutation) => nodes.concat(...mutation.addedNodes), [])
+        return this._autoRender(addedNodes);
+    }
+    static async loadCss(config) {
+        return Dlopen.loadCss(config);
+    }
+
+
     static run(message) {
-		debugger;
         const match = message.data.content.toLowerCase().match(/dtd/);
         if (match) {
             const d = new Dialog({
 				title: "Death Trap Dungeons - Shop!",
-                content: `<dtd-shop id="q-app" class="vueport-render" dependencies='dtd-shop'>Loading, please wait...</dtd-shop>`,
+                content: `<div id="app" class="dtd-app vueport-render" dependencies='magic-shop'>Loading, please wait...</div>`,
                 buttons: {}
-            }, {height: 'auto', resizable: true, popOutModuleDisable: true}).render(true);
+            }, {height: 925, width: 1500, resizable: false, popOutModuleDisable: true}).render(true);
             // Auto resize after 2 seconds
             setTimeout(() => {				
 				d.setPosition()
@@ -48,4 +97,5 @@ class DTDShop {
     }
 }
 Hooks.on('init', () => DTDShop.init());
+Hooks.on('setup', () => DTDShop.setup());
 Hooks.on('createChatMessage', (m) => DTDShop.run(m));
